4653ec49fb0669df544c14a059e5246f918b5d57
Gustavo Boaz <gustavo.boaz@hotmail.com>
2022-08-25 22:46:57 -0300

    @GustavoBoaz [Task: ADD Configure app and enviroment] {25/08}

--- seed.js
+++ seed.js
@@ -1,1 +1,13 @@
 // colocar query do MongoDB
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const mongoDbUrl = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+
+connection = await MongoClient.connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+db = connection.db('Cookmaster');
+
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database Connect
+mongoose.connect(MONGO_DB_URL);
+
+// Models
+require('./models/user.model');
+require('./models/recipe.model');
+
+// Routes
+const userRoute = require('./routes/user.routes');
+const authRoute = require('./routes/auth.routes');
+const recipeRoute = require('./routes/recipe.routes');
+
+app.use('/users', userRoute);
+app.use('/login', authRoute);
+app.use('/recipes', recipeRoute);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
